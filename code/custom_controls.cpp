c089ad49e10cbdef7fca2046db91378e819a2d47
Adarsh TS <ada@pop-os.localdomain>
2024-10-24 23:04:13 -0400

    Custom implementation of keyboard controls

--- code/custom_controls.cpp
+++ code/custom_controls.cpp
@@ -8,6 +8,7 @@ extern GLFWwindow* window; // The "extern" keyword here is to access the variabl
 using namespace glm;
 
 #include "custom_controls.hpp"
+#include <iostream>
 
 glm::mat4 c_ViewMatrix;
 glm::mat4 c_ProjectionMatrix;
@@ -37,6 +38,10 @@ float c_speed = 3.0f; // 3 units / second
 float c_mousec_speed = 0.005f;
 
 
+glm::vec3 p = glm::vec3(5, 0, 1.00);
+glm::vec3 move_speed = glm::vec3(1, 0, 0);
+glm::vec3 yaw_speed = glm::vec3(0, 1, 0);
+glm::vec3 pitch_speed = glm::vec3(0, 0, 1);
 
 void c_computeMatricesFromInputs(){
 
@@ -47,17 +52,6 @@ void c_computeMatricesFromInputs(){
 	double currentTime = glfwGetTime();
 	float deltaTime = float(currentTime - lastTime);
 
-	// Get mouse position
-	double xpos, ypos;
-	glfwGetCursorPos(window, &xpos, &ypos);
-
-	// Reset mouse position for next frame
-	glfwSetCursorPos(window, 1024/2, 768/2);
-
-	// Compute new orientation
-	c_horizontalAngle += c_mousec_speed * float(1024/2 - xpos );
-	c_verticalAngle   += c_mousec_speed * float( 768/2 - ypos );
-
 	// Direction : Spherical coordinates to Cartesian coordinates conversion
 	glm::vec3 direction(
 		cos(c_verticalAngle) * sin(c_horizontalAngle), 
@@ -67,31 +61,38 @@ void c_computeMatricesFromInputs(){
 	
 	// Right vector
 	glm::vec3 right = glm::vec3(
-		sin(c_horizontalAngle - 3.14f/2.0f), 
+		sin(p[1]), 
 		0,
-		cos(c_horizontalAngle - 3.14f/2.0f)
+		0
 	);
 	
 	// Up vector
-	glm::vec3 up = glm::cross( right, direction );
+	// glm::vec3 up = glm::rotate(glm::cross(p, glm::vec3(0, 1, 0)), -1.57);
 
 	// Move forward
-	if (glfwGetKey( window, GLFW_KEY_UP ) == GLFW_PRESS){
-		c_position += direction * deltaTime * c_speed;
+	if (glfwGetKey( window, GLFW_KEY_W ) == GLFW_PRESS){
+		p -= move_speed * deltaTime * c_speed;
 	}
 	// Move backward
-	if (glfwGetKey( window, GLFW_KEY_DOWN ) == GLFW_PRESS){
-		c_position -= direction * deltaTime * c_speed;
-	}
-	// Strafe right
-	if (glfwGetKey( window, GLFW_KEY_RIGHT ) == GLFW_PRESS){
-		c_position += right * deltaTime * c_speed;
+	if (glfwGetKey( window, GLFW_KEY_S ) == GLFW_PRESS){
+		p += move_speed * deltaTime * c_speed;
 	}
 	// Strafe left
-	if (glfwGetKey( window, GLFW_KEY_LEFT ) == GLFW_PRESS){
-		c_position -= right * deltaTime * c_speed;
+	if (glfwGetKey( window, GLFW_KEY_A ) == GLFW_PRESS){
+		p -= yaw_speed * deltaTime * c_speed;
 	}
-
+	// Strafe right
+	if (glfwGetKey( window, GLFW_KEY_D ) == GLFW_PRESS){
+		p += yaw_speed * deltaTime * c_speed;
+	}
+	// Strafe up
+	if (glfwGetKey( window, GLFW_KEY_UP ) == GLFW_PRESS){
+		p += pitch_speed * deltaTime * c_speed;
+	}
+	// Strafe down
+	if (glfwGetKey( window, GLFW_KEY_DOWN ) == GLFW_PRESS){
+		p -= pitch_speed * deltaTime * c_speed;
+	}
     if (glfwGetKey( window, GLFW_KEY_L ) == GLFW_PRESS)
     {
         if (LightMode)
@@ -99,6 +100,11 @@ void c_computeMatricesFromInputs(){
         else
             LightMode = 1;
     }
+	glm::vec3 positionInCartesian = glm::vec3 {
+		p[0] * cos(p[2]) * sin(p[1]),
+		p[0] * sin(p[2]),
+		p[0] * cos(p[1]) * cos(p[2]),
+	};
 
 	float FoV = c_initialFoV;// - 5 * glfwGetMouseWheel(); // Now GLFW 3 requires setting up a callback for this. It's a bit too complicated for this beginner's tutorial, so it's disabled instead.
 
@@ -106,11 +112,12 @@ void c_computeMatricesFromInputs(){
 	c_ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
 	// Camera matrix
 	c_ViewMatrix       = glm::lookAt(
-								c_position,           // Camera is here
-								c_position+direction, // and looks here : at the same position, plus "direction"
-								up                  // Head is up (set to 0,-1,0 to look upside-down)
+								positionInCartesian,           // Camera is here
+								glm::vec3(0,0,0), // and looks here : at the same position, plus "direction"
+								glm::vec3(0, 1, 0)                  // Head is up (set to 0,-1,0 to look upside-down)
 						   );
 
 	// For the next frame, the "last time" will be "now"
 	lastTime = currentTime;
+	// std::cout << p[1]  << " " << p[2] << std::endl;
 }
